862679ce8de0078b7ec0bf4efade1ba8ef4fe2b8
Nishant Singhal <nishantsinghal900@gmail.com>
2022-06-16 19:03:41 +0530

    Controller
    1. Exam and Subject Controller is added
    2. Routes of Subject is added
    3. Packages is added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const SubjectController = require('../controllers/subject.controller');
+const UserController = require('../controllers/user.controller');
+
+router.post("/subject", SubjectController.create);
+router.get("/subject", SubjectController.get);
+router.put("/subject", SubjectController.update);
+router.get("/subject/list", SubjectController.list);
+
+router.post('/users', UserController.create);
+router.get('/users/activation', UserController.userActivation);
+router.put('/users/updatepassword', UserController.updatePassword);
+router.post('/users/login', UserController.login);
 
 module.exports = router;
