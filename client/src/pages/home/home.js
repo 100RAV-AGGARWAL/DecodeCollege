d9ac4c18176909cc371acaf3a0307a666fff99ff
wasp0094 <90551491+wasp0094@users.noreply.github.com>
2022-06-23 22:02:37 +0530

    signup and login pages setup

---
+++ client/src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar'
+
+function Home() {
+    return (
+        <div>
+            <h1>home page</h1>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Home
