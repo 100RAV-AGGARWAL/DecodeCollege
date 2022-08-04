159b8162dc6068a8079835a38cab91fe166bd4fe
Nishant Singhal <nishantsinghal900@gmail.com>
2022-08-04 20:52:45 +0530

    Added User Frontend

--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -1,9 +1,80 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+	constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
+
+
+
+	login(username: string, password: string) {
+		let loginData = {
+			email: username,
+			password: password,
+		}
+		return new Observable((observer) => {
+			this.http.post(environment.apiUrl + 'api/users/login', loginData).subscribe(resp => {
+				observer.next(resp);
+			}, err => {
+				observer.error(err);
+			});
+		});
+	}
+
+
+
+	logout() {
+		localStorage.removeItem("role")
+		this.router.navigate(['']);
+	}
+
+	logoutUser() {
+		localStorage.removeItem("role")
+		this.router.navigate(['user/login']);
+	}
+
+	signup(signupData: {
+		username: string; password: string; first: string;
+		last: string; email: string; retypedpassword: string;
+	}) {
+		return new Observable((observer) => {
+			this.http.post(environment.apiUrl + 'api/users', signupData).subscribe(resp => {
+				observer.next(resp);
+			}, err => {
+				observer.error(err);
+			});
+		});
+	}
+
+	forgotpassword(username: any) {
+		let emailId = {
+			email: username,
+		};
+		return new Observable((observer) => {
+			this.http.post(environment.apiUrl + "api/forgot", emailId).subscribe(
+				(resp) => {
+					observer.next(resp);
+				},
+				(err) => {
+					observer.error(err);
+				}
+			);
+		});
+	}
+
+	verifytoken(token: any) {
+		return this.http.post(environment.apiUrl + "api/reset/verify", { token: token });
+	}
+
+	resetpassword(user: any) {
+		return this.http.post(environment.apiUrl + "api/reset/password", user);
+	}
+
 }
---
+++ client/src/app/user/user/userresetpassword/user-resetpassword.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { UserService } from "../../user.service";
+import { SnackBarService } from "../../../utility/snackbar/snackbar.component";
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+	selector: "app-resetpassword",
+	templateUrl: "./user-resetpassword.component.html",
+	styleUrls: ["./user-resetpassword.component.css"],
+})
+export class UserResetPasswordComponent implements OnInit {
+	user = { password: "", retypepassword: "", token: "" };
+	message = "";
+	data: any;
+	token;
+	constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private userService: UserService, private _snackBar: SnackBarService) {
+		this.token = this.route.snapshot.paramMap.get('token');
+		this.userService.verifytoken(this.token).subscribe(res => {
+			this._snackBar.openSnackBar("Please reset password", "X");
+		}, err => {
+			this._snackBar.openSnackBar("Token is invalid", "X");
+		});
+	}
+
+	ngOnInit() { }
+
+	resetpassword() {
+		//this.user.token = this.token;
+		this.userService.resetpassword(this.user).subscribe((res) => {
+			this._snackBar.openSnackBar("message", "X");
+			this.router.navigate(['/']);
+		}, (err) => {
+			this._snackBar.openSnackBar(err.error.error, "X");
+		});
+	}
+}
