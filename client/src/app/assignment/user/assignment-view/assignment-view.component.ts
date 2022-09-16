e52ef560968416b91dcd30a0eb723357689dbcd2
Sourav Aggarwal <souravagg01@gmail.com>
2022-09-16 22:12:38 +0530

    assignment view component

--- client/src/app/assignment/user/assignment-view/assignment-view.component.ts
+++ client/src/app/assignment/user/assignment-view/assignment-view.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SnackBarService } from 'src/app/utility/snackbar/snackbar.component';
+import { AssignmentService } from '../../assignment.service';
 
 @Component({
   selector: 'app-assignment-view',
   templateUrl: './assignment-view.component.html',
   styleUrls: ['./assignment-view.component.css']
 })
-export class AssignmentViewComponent implements OnInit {
+export class AssignmentViewComponent implements OnInit{
+  assignment : any;
+  filePath = '';
+  fileNameInFolder = '';
+  private sub: any;
 
-  constructor() { }
+  constructor(private _snackBar: SnackBarService, private assignmentService: AssignmentService, private router: Router, private route: ActivatedRoute) { 
+    this.sub = this.route.params.subscribe(params => {
+      this.assignmentService.getAssignment(params['id']).subscribe(resp => {
+        this.assignment = resp["assignment"];
+        this.fileNameInFolder = this.assignment.filePath.replace(/^.*[\\\/]/, '');
+        this.filePath = `assets/${this.fileNameInFolder}`;
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
