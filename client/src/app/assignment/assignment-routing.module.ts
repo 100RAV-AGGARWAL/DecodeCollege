af6352b150feb179d5d7877170514b2d77c6b4b1
Sourav Aggarwal <souravagg01@gmail.com>
2022-09-17 22:55:37 +0530

    minor updates

--- client/src/app/assignment/assignment-routing.module.ts
+++ client/src/app/assignment/assignment-routing.module.ts
@@ -6,12 +6,13 @@ import { AssignmentDeleteComponent } from './user/assignment-delete/assignment-d
 import { AssignmentMyassignmentsComponent } from './user/assignment-myassignments/assignment-myassignments.component';
 import { AssignmentEditComponent } from './user/assignment-edit/assignment-edit.component';
 import { AssignmentViewComponent } from './user/assignment-view/assignment-view.component';
+import { AuthGuard } from '../user/auth-guard.service';
 
 const assignentRoutes: Routes = [
-  {path: 'assignment/post', component: AssignmentPostComponent},
-  {path: 'assignment/delete', component: AssignmentDeleteComponent},
+  {path: 'assignment/post', component: AssignmentPostComponent, canActivate: [AuthGuard]},
+  {path: 'assignment/delete', component: AssignmentDeleteComponent, canActivate: [AuthGuard]},
   {path: 'assignment/myAssignments', component: AssignmentMyassignmentsComponent},
-  {path: 'assignment/edit/:id', component: AssignmentEditComponent},
+  {path: 'assignment/edit/:id', component: AssignmentEditComponent, canActivate: [AuthGuard]},
   {path: 'assignment/view/:id', component: AssignmentViewComponent},
 ];
 
