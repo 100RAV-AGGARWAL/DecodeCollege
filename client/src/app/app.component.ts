93b78c4d0cf58c4a54589fcb993c5c35614e8e15
Varun Narang <varunnarang01@gmail.com>
2022-09-30 14:32:48 +0530

    timeout-3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,32 +6,27 @@ import { Component, HostListener, OnDestroy } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy{
+export class AppComponent implements OnDestroy {
   title = 'client';
   isTimeOut = false;
   timepassed: string = ''
   num: number = 0
   count: number = 0
   count2: number = 0
-  limit: number = 60000;
-  upperLimit: number = 90000;
+  limit: number = 300000;
+  upperLimit: number = 330000; 
   timeLeft: number = this.limit
   timeLeft2: number = this.upperLimit - this.limit;
-  ngOnDestroy(){
+  
+  ngOnDestroy() {
     localStorage.removeItem('time');
   }
-  
+
   @HostListener("window:beforeunload", ["$event"]) unloadHandler(event: Event) {
-   //  if(!this.isTimeOut){
-      this.num = this.num + this.count+this.count2;
-      this.timepassed = (this.num).toString();
-      localStorage.setItem('time', this.timepassed);
-      console.log(this.timepassed);
-   //  }
-
-
-    // Do more processing...
-
+    this.num = this.num + this.count + this.count2;
+    this.timepassed = (this.num).toString();
+    localStorage.setItem('time', this.timepassed);
+    console.log(this.timepassed);
   }
 
   constructor() {
@@ -47,22 +42,16 @@ export class AppComponent implements OnDestroy{
       this.isTimeOut = false;
       this.timeLeft = this.limit - this.num;
     }
-    else if (this.num >= this.limit && this.num<this.upperLimit) {
+    else if (this.num >= this.limit && this.num < this.upperLimit) {
       this.timeLeft = 0;
       this.timeLeft2 = this.upperLimit - this.num;
     }
-    else if(this.num>=this.upperLimit){
-      localStorage.setItem('time','0');
-      this.num=0;
+    else if (this.num >= this.upperLimit) {
+      localStorage.setItem('time', '0');
+      this.num = 0;
 
     }
 
-    
-
-    //  this.num=this.num-1;
-
-
-    //setTimeout(()=>{this.isTimeOut=true,this.displayscreen()},this.timeLeft);
     let IntervalId = setInterval(() => {
       if (this.count != this.timeLeft) {
         this.count += 1000;
@@ -75,25 +64,20 @@ export class AppComponent implements OnDestroy{
         clearInterval(IntervalId);
       }
     }, 1000);
-
   }
+
   displayscreen() {
     if (this.isTimeOut) {
-     //  setTimeout(()=>{this.isTimeOut=false,localStorage.setItem('time','0'),this.num=0,this.count=0},5000);
-
       let Intid = setInterval(() => {
         if (this.count2 != this.timeLeft2) {
           this.count2 += 1000;
         } else {
           this.isTimeOut = false;
           localStorage.setItem('time', '0');
+          window.location.reload();
           clearInterval(Intid);
         }
-
       }, 1000)
-
-      // localStorage.setItem('time','0');
-      // this.num=0;
     }
   }
 }
