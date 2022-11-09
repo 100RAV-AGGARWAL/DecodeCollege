66cc0611567c5835304922f982a7d69828b5558f
Sourav Aggarwal <souravagg01@gmail.com>
2022-11-09 19:22:38 +0530

    production changes

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,9 +1,13 @@
 export const environment = {
   production: true,
   apiUrl: "http://localhost:3000/",
+	advertisment: false,
   customSearchAPIKey: "AIzaSyCBb0HR-KCeg7H0WPaZHcXXpUW1zDiX8zY",
   programmableSearchEngineId: "b3a26e3274cd8420f",
   customSearchUrl: "https://customsearch.googleapis.com/customsearch/v1",
+  paraphrasingKey: "76a9956a2bmshc7910f6b39794ebp10e4f1jsnffe6a50334d4",
+  paraphrasingUrl: "https://paraphraser1.p.rapidapi.com/",
+  paraphrasingHost: "paraphraser1.p.rapidapi.com/",
   firebaseConfig: {
     apiKey: "AIzaSyC8etMYB8s9E-cd6BUvr6TA70itbDYTXVk",
     authDomain: "decodecollege.firebaseapp.com",
