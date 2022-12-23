1a9398553b6877232a68c96039785aeb37c82a08
Sourav Aggarwal <souravagg01@gmail.com>
2022-12-23 11:05:47 +0530

    railway deployment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,20 +1,20 @@
 export const environment = {
-  production: true,
-  apiUrl: "https://decode-college.herokuapp.com/",
+	production: true,
+	apiUrl: "https://decodecollege.up.railway.app/",
 	advertisment: false,
-  customSearchAPIKey: "AIzaSyCBb0HR-KCeg7H0WPaZHcXXpUW1zDiX8zY",
-  programmableSearchEngineId: "b3a26e3274cd8420f",
-  customSearchUrl: "https://customsearch.googleapis.com/customsearch/v1",
-  paraphrasingKey: "76a9956a2bmshc7910f6b39794ebp10e4f1jsnffe6a50334d4",
-  paraphrasingUrl: "https://paraphraser1.p.rapidapi.com/",
-  paraphrasingHost: "paraphraser1.p.rapidapi.com/",
-  firebaseConfig: {
-    apiKey: "AIzaSyC8etMYB8s9E-cd6BUvr6TA70itbDYTXVk",
-    authDomain: "decodecollege.firebaseapp.com",
-    projectId: "decodecollege",
-    storageBucket: "decodecollege.appspot.com",
-    messagingSenderId: "49851792422",
-    appId: "1:49851792422:web:a28388239cbd07fb89bae7",
-    measurementId: "G-RXRBMN6J4P"
-  },
+	customSearchAPIKey: "AIzaSyCBb0HR-KCeg7H0WPaZHcXXpUW1zDiX8zY",
+	programmableSearchEngineId: "b3a26e3274cd8420f",
+	customSearchUrl: "https://customsearch.googleapis.com/customsearch/v1",
+	paraphrasingKey: "76a9956a2bmshc7910f6b39794ebp10e4f1jsnffe6a50334d4",
+	paraphrasingUrl: "https://paraphraser1.p.rapidapi.com/",
+	paraphrasingHost: "paraphraser1.p.rapidapi.com/",
+	firebaseConfig: {
+		apiKey: "AIzaSyC8etMYB8s9E-cd6BUvr6TA70itbDYTXVk",
+		authDomain: "decodecollege.firebaseapp.com",
+		projectId: "decodecollege",
+		storageBucket: "decodecollege.appspot.com",
+		messagingSenderId: "49851792422",
+		appId: "1:49851792422:web:a28388239cbd07fb89bae7",
+		measurementId: "G-RXRBMN6J4P"
+	},
 };
